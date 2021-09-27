6cd95ed6a369f8cbeb18e23a6b08379de515a29c
Christopher Burwell <crburwell@yahoo.com>
2021-09-27 02:28:24 +0000

    more commands

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ import os
 import discord
 import requests
 import json
-import random
+from discord.ext import commands
 from replit import db
 from keep_alive import keep_alive
+from music import Player
 
 client = discord.Client()
 
-sus_words = ["sussy", "Sussy", "sus", "Sus", "SUS"]
+intents = discord.Intents.default()
+intents.members = True
 
-starter_sussy_replies = [
-  "You Sussy little Baka!",
-  "When imposter is SUS??!",
-  "SUSSY AMOGUS?"
-]
+bot = commands.Bot(command_prefix="$", intents=intents)
 
 if "responding" not in db.keys():
   db["responding"] = True
@@ -25,19 +23,6 @@ def get_quote():
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
 
-def update_sussy_replies(sussy_reply):
-  if "sussy_replies" in db.keys():
-    sussy_replies = db["sussy_replies"]
-    sussy_replies.append(sussy_reply)
-    db["sussy_replies"] = sussy_replies
-  else:
-    db["sussy_replies"] = [sussy_reply]
-
-def delete_sussy_reply(index):
-  sussy_replies = db["sussy_replies"]
-  if len(sussy_replies) > index:
-    del sussy_replies[index]
-    db["sussy_replies"] = sussy_replies
 
 def get_waifu():
   response = requests.get("https://api.waifu.pics/sfw/wink")
@@ -45,18 +30,19 @@ def get_waifu():
   waifu = "*LOADING WINK...* \n " + json_data['url']
   return(waifu)
 
-@client.event
-async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+# @client.event
+# async def on_ready():
+#   print('We have logged in as {0.user}'.format(client))
 
-TOKEN = os.environ['key']
+@bot.event
+async def on_ready():
+  print("bot is ready.")
+
 
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
-
-  msg = message.content
 
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
@@ -65,48 +51,15 @@ async def on_message(message):
     quote = get_quote()
     await message.channel.send(quote)
 
-  if db["responding"]:
-    options = starter_sussy_replies
-    if "sussy_replies" in db.keys():
-      options.extend(db["sussy_replies"])
-      # options = options + db["sussy_replies"]
-
-    if any(word in msg for word in sus_words):
-      await message.channel.send(random.choice(options))
-
-  if msg.startswith("$new"):
-    sussy_reply = msg.split("$new ",1)[1]
-    update_sussy_replies(sussy_reply)
-    await message.channel.send("New reply added")
-
-  if msg.startswith("$del"):
-    sussy_replies = []
-    if "sussy_replies" in db.keys():
-      index = int(msg.split("$del",1)[1])
-      delete_sussy_reply(index)
-      sussy_replies = db["sussy_replies"]
-    await message.channel.send(sussy_replies)
-
-  if msg.startswith("$list"):
-    sussy_replies = []
-    if "sussy_replies" in db.keys():
-      sussy_replies = db["sussy_replies"]
-    await message.channel.send(sussy_replies)
-
-  if msg.startswith("$responding"):
-    value = msg.split("$responding ",1)[1]
-
-    if value.lower() =="true":
-      db["responding"]= False
-      await message.channel.send("Responding is on")
-    else:
-      db["responding"]= False
-      await message.channel.send("Responding is off")
-
   if message.content.startswith('$wink'):
     waifu = get_waifu()
     await message.channel.send(waifu)
 
 # this keeps the webserver running
 keep_alive()
+#add music player cog
+bot.add_cog(Player(bot))
+#Use TOKEN variable to run bot (keep secret)
+TOKEN = os.environ['key']
 client.run(TOKEN)
+bot.run(TOKEN)
